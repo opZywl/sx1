abe30c78900ff5f335975a409f08c4d595c6f3cf
Almeida <kauanascimento2016007@gmail.com>
2026-01-17 22:57:37 -0300

    Imagem do brasil, alteração do nome da pag

--- sx1/src/sx1frontend/components/Nike.jsx
+++ sx1/src/sx1frontend/components/Nike.jsx
@@ -16,7 +16,7 @@ const Nike = () => {
           className="-top-40 left-0 md:left-60 md:-top-20"
           fill="white"
         />
-        <div className="absolute top-10 left-10">
+        <div className="absolute top-10 left-10 z-20">
           <motion.div
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
@@ -24,7 +24,7 @@ const Nike = () => {
             className="font-mono font-extrabold lg:text-[7rem] text-6xl"
           >
             <h2 className="bg-gradient-to-b from-zinc-200 font-mono to-zinc-600 bg-clip-text leading-none text-transparent">
-              Nike Kobe X All Star Edition{" "}
+              SX1 IMPORTS{" "}
             </h2>
           </motion.div>
           <motion.p
@@ -33,7 +33,7 @@ const Nike = () => {
             transition={{ duration: 0.5, delay: 1 }}
             className="text-3xl font-bold font-mono bg-white text-black w-[70%]"
           >
-            Ataque com precisão.
+            TOP 01 EM IMPORTS
           </motion.p>
         </div>
         <motion.div
@@ -42,11 +42,13 @@ const Nike = () => {
           transition={{ duration: 1, delay: 1.5 }}
           className="absolute w-1 font-bold font-mono flex-col flex max-sm:flex-row  max-sm:w-max max-sm:bottom-10 max-sm:left-10 items-center justify-center right-20 bottom-56 text-3xl"
         >
-          <span>M</span>
+          <span>C</span>
           <span>A</span>
           <span>M</span>
-          <span>B</span>
+          <span>I</span>
+          <span>S</span>
           <span>A</span>
+          <span>S</span>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, y: 40 }}
@@ -54,24 +56,25 @@ const Nike = () => {
           transition={{ duration: 1, delay: 1.75 }}
           className="absolute w-1 font-bold font-mono flex-col max-sm:flex-row  max-sm:w-max max-sm:bottom-10 flex items-center justify-center right-10 bottom-5 text-3xl"
         >
-          <span>M</span>
-          <span>E</span>
-          <span>N</span>
-          <span>T</span>
-          <span>A</span>
+          <span>W</span>
+          <span>O</span>
+          <span>R</span>
+          <span>D</span>
+          <span>&nbsp;</span>
+          <span>C</span>
           <span>L</span>
-          <span>I</span>
-          <span>T</span>
-          <span>Y</span>
+          <span>A</span>
+          <span>S</span>
+          <span>S</span>
         </motion.div>
         
         <motion.img
           initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1, delay: 2.25 }}
-          src="/images/hero.png"
+          src="/images/brasil.png"
           alt=""
-          className="w-[100%] object-contain z-10 max-sm:my-36"
+          className="w-[100%] object-contain z-10 max-sm:my-36 mt-48"
         />
       </div>
     </div>
